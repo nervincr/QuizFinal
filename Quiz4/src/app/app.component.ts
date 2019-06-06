8560c2ee298f347f68408c4c500487e17ddabb5e
nervincr <nervincr@users.noreply.github.com>
2019-06-05 23:48:52 -0600

    Quiz Listo

--- Quiz4/src/app/app.component.ts
+++ Quiz4/src/app/app.component.ts
@@ -4,16 +4,8 @@ interface Sitio {
   img: string;
   nombre: string;
   descripcion: string;
+  ocultar: boolean;
 }
-
-let items: Sitio[] = [
-{img:'', nombre:'',descripcion:''},
-{img:'', nombre:'',descripcion:''},
-{img:'', nombre:'',descripcion:''},
-{img:'', nombre:'',descripcion:''},
-{img:'', nombre:'',descripcion:''},
-{img:'', nombre:'',descripcion:''}];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,4 +13,35 @@ let items: Sitio[] = [
 })
 export class AppComponent {
   title = 'Quiz4';
+  items: Sitio[];
+  constructor() {
+    this.reset();
+  }
+
+  ocultar(id: number) {
+    this.items[id].ocultar = true;
+  }
+
+  reset() {
+    this.items = [
+      {img: 'https://cdn.worldvectorlogo.com/logos/facebook-2.svg',
+       nombre: 'Instagram', descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+       ocultar: false},
+      {img: 'https://cdn.worldvectorlogo.com/logos/facebook-2.svg',
+       nombre: 'Twitter', descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+       ocultar: false},
+      {img: 'https://cdn.worldvectorlogo.com/logos/facebook-2.svg',
+       nombre: 'Facebook', descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+       ocultar: false},
+      {img: 'https://cdn.worldvectorlogo.com/logos/facebook-2.svg',
+       nombre: 'Pinterest', descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+       ocultar: false},
+      {img: 'https://cdn.worldvectorlogo.com/logos/facebook-2.svg',
+       nombre: 'Google', descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+       ocultar: false},
+      {img: 'https://cdn.worldvectorlogo.com/logos/facebook-2.svg',
+       nombre: 'Github', descripcion: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+       ocultar: false}
+    ];
+  }
 }
